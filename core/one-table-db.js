24e0da608de3ba111b1ca8ee8cc86652cfa8bbf1
orey <rey.olivier@gmail.com>
2021-11-13 10:16:34 +0100

    intermediate commit

---
+++ core/one-table-db.js
@@ -0,0 +1,65 @@
+/**********************************************
+ * One table DB
+ * Author: rey.olivier@gmail.com
+ * Licence: GNU GPL v3
+ * Date: November 2021
+ * This is a simple storage system aiming at 
+ * storing one table and emulating simple graph
+ * behavior
+ * expected file extension is "otdb"
+ ***********************************************/
+"use strict";
+const fs = require('fs')
+
+const SEP='|';
+const COMMENT='//';
+
+
+
+function readDatabase(filename, verbose=false) {
+    try {
+        // read contents of the file
+        const data = fs.readFileSync(filename, 'UTF-8');
+
+        // split the contents by new line
+        const lines = data.split(/\r?\n/);
+        
+        // print all lines
+        lines.forEach((line) => {
+            if ((line.startsWith(COMMENT)) && verbose) 
+                console.log("Excluding comment:\n%s",line);
+            else if ((line.trim == '') && verbose)
+                console.log("Found blank line. Skipping...");
+            else {
+                let tab = line.split(SEP);
+                // line is supposed to be a triple
+                if (tab.length != 3) 
+                    console.log("Malformed line (not 3 separators).\n%s\nSkipping...", line);
+                else
+                    //console.log(tab);
+                    console.log("Line: %s - %s - %s", tab[0], tab[1], tab[2]);
+            }
+        });
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+
+/*****************************************/
+function test() {
+    readDatabase("data.otdb", true);
+}
+
+test()
+
+/*****************************************/
+
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = {
+        test,
+    }
+}
+
+
