8949cc775916b33c0f9c6210983b322636aa5d46
orey <rey.olivier@gmail.com>
2021-11-14 10:01:32 +0100

    first version of the one table db

--- core/one-table-db.js
+++ core/one-table-db.js
@@ -13,54 +13,128 @@ const fs = require('fs')
 
 const SEP = '|';
 const COMMENT = '//';
+const QUESTION = '?';
 
-let DB = [];
+class OneTableDB {
+    constructor(filename, verbose=false) {
+        this.DB = [];
+        try {
+            // read contents of the file
+            const data = fs.readFileSync(filename, 'UTF-8');
 
-
-
-function readDatabase(filename, verbose=false) {
-    try {
-        // read contents of the file
-        const data = fs.readFileSync(filename, 'UTF-8');
-
-        // split the contents by new line
-        const lines = data.split(/\r?\n/);
+            // split the contents by new line
+            const lines = data.split(/\r?\n/);
         
-        let i = 0;
-        // print all lines
-        lines.forEach((line) => {
-            if ((line.startsWith(COMMENT)) && verbose) 
-                console.log("Excluding comment:\n%s",line);
-            else if ((line.trim == '') && verbose)
-                console.log("Found blank line. Skipping...");
-            else {
-                let tab = line.split(SEP);
-                // line is supposed to be a triple
-                if (tab.length != 3) 
-                    console.log("Malformed line (not 3 separators).\n%s\nSkipping...", line);
+            let i = 0;
+            // print all lines
+            lines.forEach((line) => {
+                if ((line.startsWith(COMMENT)) && verbose) 
+                    console.log("Excluding comment:\n%s",line);
+                else if ((line.trim == '') && verbose)
+                    console.log("Found blank line. Skipping...");
                 else {
-                    //console.log(tab);
-                    console.log("Line: %s - %s - %s", tab[0], tab[1], tab[2]);
-                    DB[i] = tab;
-                    i += 1;
+                    let tab = line.split(SEP);
+                    // line is supposed to be a triple
+                    if (tab.length != 3) 
+                        console.log("Malformed line (not 3 separators).\n%s\nSkipping...", line);
+                    else {
+                        //console.log(tab);
+                        if (verbose)
+                            console.log("Line: %s - %s - %s", tab[0], tab[1], tab[2]);
+                        this.DB[i] = tab;
+                        i += 1;
+                    }
                 }
+                if (verbose)
+                    console.log(this.DB);
+            });
+        }
+        catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+
+    /*****************************************/
+
+    /*
+      For the moment the query is simple: variables are accepted in the triple
+    */
+    select(s, p, o) {
+        let variables = 0;
+        if (s.startsWith(QUESTION))
+            variables += 4; // 100
+        if (p.startsWith(QUESTION))
+            variables += 2; // 010
+        if (o.startsWith(QUESTION))
+            variables += 1; // 001
+        let len = this.DB.length;
+        let line = "";
+        let results = [];
+        for (let i=0;i<len;i++) {
+            line = this.DB[i];
+            switch (variables) {
+            case 1: 
+                // 001 value value ?var
+                if ((line[0] == s) && (line[1] == p))
+                    results.push(line);
+                break;
+            case 2:
+                // 010 value ?var value
+                if ((line[0] == s) && (line[2] == o))
+                    results.push(line);
+                break;
+            case 4:
+                // 100 ?var value value
+                if ((line[1] == p) && (line[2] == o))
+                    results.push(line);
+                break;
+            case 3:
+                // 011 value ?var ?var
+                if (line[0] == s)
+                    results.push(line);
+                break;
+            case 5:
+                // 101 ?var value ?var
+                if (line[1] == p)
+                    results.push(line);
+                break;
+            case 6:
+            // 110 ?var ?var value
+                if (line[2] == 0)
+                    results.push(line);
+                break;
+            case 7:
+                // 111 ?var ?var ?var
+                results.push(line);
+                break;
             }
-            console.log(DB);
-        });
+        }
+        return results;
     }
-    catch (err) {
-        console.error(err);
-    }
-}
-
-function query(subject, predicate, object) {
-
 }
 
 
 /*****************************************/
 function test() {
-    readDatabase("data.otdb", true);
+    let db = new OneTableDB("data.otdb", false);
+    console.log("001")
+    console.log(db.select("Tombouctou", "a", "?"));
+    console.log("010")
+    console.log(db.select("Hero", "?", "a big sword"));
+    console.log("100")
+    console.log(db.select("?", "a", "City"));
+    console.log("011")
+    console.log(db.select("Paris", "?", "?"));
+    console.log("101")
+    console.log(db.select("?", "has", "?"));
+    console.log("110")
+    console.log(db.select("a dream", "a","?"));
+    console.log("111")
+    console.log(db.select("?", "?", "?"));
+    let results = db.select("?", "a", "turtle");
+    console.log(results.length)
+    
 }
 
 test()
